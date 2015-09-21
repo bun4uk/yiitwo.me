bde81354e4e1cbdfdf49663dfd6473b924640136
x-bvs <380957575275@ya.ru>
2015-09-22 00:59:16 +0300

    yii2 init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiitwo',
+    'username' => 'vova',
+    'password' => '7895521v',
+    'charset' => 'utf8',
+];
